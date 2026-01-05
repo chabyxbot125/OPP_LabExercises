36d126bab86fe5f665fab37cfe5d9907332a2c7a
2024_1793 <chabylabasano8@gmail.com>
2026-01-05 21:56:41 +0800

    Create Expected Terminal Output.java

---
+++ EXER-4/Expected Terminal Output.java
@@ -0,0 +1,10 @@
+C:\Users\Student\Documents\Exer4> javac ProjectTester.java
+C:\Users\Student\Documents\Exer4> java ProjectTester
+
+=== TESTING METHOD OVERLOADING (Compile-time) ===
+Match found by Title: Java Mastery
+Match found by ID: #101 (Java Mastery)
+
+=== TESTING METHOD OVERRIDING (Runtime) ===
+[BOOK] ID: 101 | Title: Java Mastery | Author: James Gosling
+[E-BOOK] ID: 202 | Title: Future of AI | Format: PDF | Size: 12.5MB
